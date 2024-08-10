867e06f64fc638bfbc875f4faaa865353a44eee2
holyholic704 <1031557588@qq.com>
2024-08-10 16:43:37 +0800

    perf: 项目结构调整

--- doro-core/src/main/java/com/doro/core/config/IdGeneratorInit.java
+++ doro-core/src/main/java/com/doro/core/config/IdGeneratorInit.java
@@ -2,6 +2,7 @@ package com.doro.core.config;
 
 import com.github.yitter.contract.IdGeneratorOptions;
 import com.github.yitter.idgen.YitIdHelper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -9,9 +10,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class IdGeneratorInit implements ApplicationRunner {
 
+    @Value("${IdGenerator.workId}")
+    private short workId;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        IdGeneratorOptions options = new IdGeneratorOptions((short) 1);
+        IdGeneratorOptions options = new IdGeneratorOptions(workId);
         YitIdHelper.setIdGenerator(options);
         System.out.println("id 初始化");
     }
