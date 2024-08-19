0ee72c7c9c993eabd6a167cfb740323dab92a5c6
holyholic704 <1031557588@qq.com>
2024-08-19 10:14:41 +0800

    perf: 缓存优化

--- doro-cache/src/main/java/com/doro/cache/flush/FlushLocalCacheListener.java
+++ doro-cache/src/main/java/com/doro/cache/flush/FlushLocalCacheListener.java
@@ -2,14 +2,12 @@ package com.doro.cache.flush;
 
 import com.doro.cache.utils.LocalCacheUtil;
 import org.redisson.api.listener.MessageListener;
-import org.springframework.stereotype.Component;
 
 /**
  * 消息监听器，刷新本地缓存
  *
  * @author jiage
  */
-@Component
 class FlushLocalCacheListener implements MessageListener<String> {
 
     @Override
